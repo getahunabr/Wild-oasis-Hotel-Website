7e856500f0fab006e9ba648ec0ff6bbc3a4e0e21
getahunabr <147873183+getahunabr@users.noreply.github.com>
2024-08-25 00:23:47 -0700

    updated

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -3,7 +3,7 @@ import { useRecentBookings } from "./useRecentBookings";
 import Spinner from "../../ui/Spinner";
 import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
-import { useCabins } from "../cabins/useCabins.js";
+import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
 import TodayActivity from "../check-in-out/TodayActivity";
